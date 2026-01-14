aa8f97b244479263c370e5546327986cd1e60b6f
phearomkh09 <khkha8410@gmail.com>
2026-01-14 22:01:54 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -30,3 +30,30 @@ hamburger.addEventListener("click", () => {
   menu.classList.toggle("show");
   hamburger.classList.toggle("active");
 });
+
+let slides = document.querySelectorAll(".slide");
+let dots = document.querySelectorAll(".dot");
+let index = 0;
+
+function showSlide(i) {
+  slides.forEach(slide => slide.classList.remove("active"));
+  dots.forEach(dot => dot.classList.remove("active"));
+
+  slides[i].classList.add("active");
+  dots[i].classList.add("active");
+}
+
+function autoSlide() {
+  index++;
+  if (index >= slides.length) index = 0;
+  showSlide(index);
+}
+
+dots.forEach((dot, i) => {
+  dot.addEventListener("click", () => {
+    index = i;
+    showSlide(index);
+  });
+});
+
+setInterval(autoSlide, 4000);
