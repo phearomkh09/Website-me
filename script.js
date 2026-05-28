9fda3f309ff1fb1cca8747d624e6b73e7222bed7
X'zen-offlcial <khkha8410@gmail.com>
2026-05-29 06:26:16 +0700

    Implement snow effect in script.js
    Added a snow effect that creates snowflakes at random positions and sizes.

--- script.js
+++ script.js
@@ -34,3 +34,36 @@ setInterval(()=>{
   showSlide(current);
 
 },3000);
+
+/* SNOW EFFECT */
+
+const snowContainer =
+document.querySelector(".snow-container");
+
+function createSnow(){
+
+  const snow =
+  document.createElement("div");
+
+  snow.classList.add("snowflake");
+
+  snow.innerHTML = "❄";
+
+  snow.style.left =
+  Math.random() * window.innerWidth + "px";
+
+  snow.style.fontSize =
+  Math.random() * 20 + 10 + "px";
+
+  snow.style.animationDuration =
+  Math.random() * 3 + 2 + "s";
+
+  snowContainer.appendChild(snow);
+
+  setTimeout(() => {
+    snow.remove();
+  },5000);
+
+}
+
+setInterval(createSnow,100);
